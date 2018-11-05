6c85d3011adfa287d13789d5787dc2beee3a9456
Benny Franco Dennis <bennyfranco@icloud.com>
2018-11-04 22:50:46 -0800

    Adding frameworks

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-gcc "ldengine.c" -I/usr/local/include -L/usr/local/lib -F/Users/bennyfrancodennis/Library/frameworks -framework SDL -o build/LDEngine
+gcc "ldengine.c" -I/usr/local/include -L/usr/local/lib -F./frameworks -framework SDL -o build/LDEngine
 open build/LDEngine
